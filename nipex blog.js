60cadf774ec64767550980e6b71a0ea9e8c4190c
nipexhere <hadebayorhusseini@gmail.com>
2024-12-19 05:40:46 +0100

    first commit

---
+++ nipex blog.js
@@ -0,0 +1,54 @@
+<script>
+document.write("nipex blog");
+window.alert("welcome to my blog");
+console.log('nipex blog is doing fine')
+
+var _nip =(10)
+var x = (_nip)
+document.write (x)
+
+
+//single line comment 
+
+
+/*multi line comment*/
+
+var price =(55.55)
+document.write (price)
+
+
+var name ='nipex';
+var text ='i am "nipex"your only afro emo artiste';
+
+document.write (text)
+
+var isonline=true;
+var isoffline=false;
+
+var c = 10+5;
+document.write(c)
+var y= c + 5 +10 ;
+document.write (y)
+
+// +, -, *, /, %, ++ (increment), --(decrement) 
+var a = 10; a++; 
+document.write(a)
+
+function nipexfunction () {
+  x = document.getElementById('demo')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</script>
