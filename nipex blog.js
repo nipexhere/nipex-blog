00ffd267b82691439403353047535b223b20ab3f
nipexhere <hadebayorhusseini@gmail.com>
2024-12-22 16:03:56 +0100

    New commit

--- nipex blog.js
+++ nipex blog.js
@@ -4,13 +4,6 @@ document.write("nipex blog");
 window.alert("welcome to my blog");
 console.log('nipex blog is doing fine')
 
-/* var _nip =(10)
-var x = (_nip) 
-document.write (x)
-var price =(55.55)
-document.write (price)
-
-
 var name ='nipex';
 var text ='i am "nipex"your only afro emo artiste';
 
